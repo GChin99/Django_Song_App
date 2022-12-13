0ae7fe618507ff05380e5123cf74a20aec7d1ad2
Gchin99 <gchin99@gmail.com>
2022-12-13 09:19:03 -0800

    created class

--- songApp/models.py
+++ songApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    album_name = models.CharField()
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
