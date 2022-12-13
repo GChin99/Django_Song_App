843399efd8f4a4d81ca8705a82bd00c52863c016
Gchin99 <gchin99@gmail.com>
2022-12-13 09:27:01 -0800

    created DB table

--- songApp/models.py
+++ songApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Album(models.Model):
-    album_name = models.CharField()
+    album_name = models.CharField(max_length=100)
     year = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
